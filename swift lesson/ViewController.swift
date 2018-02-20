46b4131b038b57863e0bc2610ab103e67870cef3
Brandon Romans <>
2018-02-20 14:13:09 -0500

    my first commit

--- swift lesson/ViewController.swift
+++ swift lesson/ViewController.swift
@@ -9,7 +9,19 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var label: UILabel!
+    var tapCount = 0
+    @IBAction func buttonpress(_ sender: Any)
+    {label.text = "hello brandon"
+        tapCount = tapCount + 1
+        
+        if tapCount >= 10 {
+            label.text = "You have tapped the button 10 times!"
+            
+        }
+        
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
